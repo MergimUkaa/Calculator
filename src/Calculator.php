28ce1984013ec305f4e9ba6dd1d31e952f6d12c1
mergimuka-psih <mergimuka1@gmail.com>
2020-07-16 09:05:03 +0200

    change homepage link in composer.json

--- src/Calculator.php
+++ src/Calculator.php
@@ -4,5 +4,36 @@ namespace MergimU\Calculator;
 
 class Calculator
 {
-    // Build wonderful things
+    private $result;
+
+    public function __construct()
+    {
+        $this->result = 0;
+    }
+
+    public function add(int $value)
+    {
+        $this->result += $value;
+
+        return $this;
+    }
+
+    public function subtract(int $value)
+    {
+        $this->result -= $value;
+
+        return $this;
+    }
+
+    public function clear()
+    {
+      $this->result = 0;
+
+      return $this;
+    }
+
+    public function getResult()
+    {
+        return $this->result;
+    }
 }
